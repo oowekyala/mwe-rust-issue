695295e0c513a50cf644aefb076e801401e1155c
Clément Fournier <clement.fournier76@gmail.com>
2021-09-27 17:20:23 +0200

    Now it's minimal

--- benches/some_benchmark.rs
+++ benches/some_benchmark.rs
@@ -1,17 +1,8 @@
-use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main, black_box};
-use std::time::Duration;
+use criterion::{Criterion, criterion_group, criterion_main, black_box};
 
-criterion_group!(benches, reactor_main);
+criterion_group!(benches, my_main);
 criterion_main!(benches);
 
-fn reactor_main(c: &mut Criterion) {
-    let mut group = c.benchmark_group("savina_pong");
-    for num_pongs in [1000, 10_000, 50_000, 1_000_000].iter() {
-        group.bench_with_input(
-            BenchmarkId::from_parameter(num_pongs),
-            num_pongs,
-            |b, &size| b.iter(|| black_box(size)),
-        );
-    }
-    group.finish();
+fn my_main(c: &mut Criterion) {
+    black_box(c);
 }
